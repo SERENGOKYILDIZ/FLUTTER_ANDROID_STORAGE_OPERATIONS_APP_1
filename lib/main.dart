9777889f7d7aba93336e87b84fc7476611072f4c
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-11-14 15:45:20 +0300

    finish app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +27,27 @@ class Anasayfa extends StatefulWidget {
 }
 
 class _AnasayfaState extends State<Anasayfa> {
+
+  int sayac = 0;
+
+  Future<void> sayacKontrol() async {
+    var sp = await SharedPreferences.getInstance();
+
+    sayac = sp.getInt("sayac") ?? 0;
+
+    setState(() {
+      sayac++;
+    });
+
+    sp.setInt("sayac", sayac);
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    sayacKontrol();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +59,7 @@ class _AnasayfaState extends State<Anasayfa> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
+            Text("Sayac : ${sayac}", style: TextStyle(fontSize: 50),),
           ],
         ),
       ),
